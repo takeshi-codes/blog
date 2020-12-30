cf169908a22beaebc912c252db780c1d11b64881
CtrlAltDylan <dylan@ctrlaltdylan.com>
2020-12-29 21:54:43 -0600

    feat: updates to homepage, styling changes

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const ImageContainer = styled(props => <Img {...props} />)`
+  max-width: 15vw;
+  border-radius: 50%;
+  margin: 3vh auto;
+  @media (max-width: 768px) {
+    max-width: 50vw;
+  }
+`;
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -16,9 +26,9 @@ import Img from 'gatsby-image';
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "profile.jpeg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 3000) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -30,7 +40,7 @@ const Image = () => {
     return <div>Picture not found</div>;
   }
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
+  return <ImageContainer fluid={data.placeholderImage.childImageSharp.fluid} />;
 };
 
 export default Image;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import Blogs from '../components/blogs';
+import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <>
     <Layout>
-      <SEO title="Blog" />
+      <SEO/>
+      <Image />
       <h1>Hey, I'm Dylan</h1>
       <p>I like to make things, welcome to my corner of the internet</p>
-      <h2>&#60;Blogs&#47;&#62;</h2>
       <Blogs />
   </Layout>
   </>
