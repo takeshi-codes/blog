c3924f1527823b0b3ea824c068b99cbd80ac8be0
its-dgreen <dylan@ctrlaltdylan.com>
2021-01-06 21:00:04 -0600

    refactor: update image loading

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -12,24 +12,13 @@ const ImageContainer = styled(props => <Img {...props} />)`
   }
 `;
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "profile.jpeg" }) {
         childImageSharp {
-          fluid(maxWidth: 3000, quality: 90) {
-            ...GatsbyImageSharpFluid
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
